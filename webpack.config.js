9cb77972cf2e7b618c4bc04b413b99e55d6cc7c2
Andrea <andrearivas4cinfor@gmail.com>
2023-01-28 13:06:28 +0100

    solving html problem

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require.resolve('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
