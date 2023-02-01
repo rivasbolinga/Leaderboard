4aa88ab0ba3e12da23fa3ad5dd4988154f48dc06
Andrea <andrearivas4cinfor@gmail.com>
2023-02-01 11:46:14 +0100

    display scores

--- src/index.js
+++ src/index.js
@@ -9,21 +9,33 @@ const id = 'N6oJbbERy2ToK2k4n7vD';
 
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
-
+const leaderboard = document.querySelector('.leaderboard')
 
 
 //-- Method to retrieve the data from the API
 const getData = async () => {
   const response = await fetch(`${url}games/${id}/scores`)
   const data = await response.json();
-  console.log(data);
+  return data;
+ }
+
+ const displayScores = async () => {
+  const scores = await getData();
+  const scoresObj = scores.result;
+  scoresObj.forEach((score) => {
+    const scoreContainer = document.createElement('div');
+    scoreContainer.classList.add('score-container');
+    scoreContainer.innerHTML = `${score.user}: ${score.score}`;
+   leaderboard.appendChild(scoreContainer)
+  });
  }
 //-- When press Refresh btn, make a request to ger the data from API.
 refreshBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  getData()
+  displayScores();
 });
 
+//-- When form is completed, sends the inputs to the API. 
 const submitScores = async (user,score) => {
   const response = await fetch(`${url}games/${id}/scores`, {
       method: 'POST',
@@ -33,14 +45,19 @@ const submitScores = async (user,score) => {
       body: JSON.stringify({ user,score}),
   });
   const data = await response.json();
-  console.log(data)
+  return data;
 }
 
+const clearfields = () => {
+  nameInput.value = '';
+  scoreInput.value = '';
+}
 
-submitBtn.addEventListener('click', (e) => {
+submitBtn.addEventListener('click', async (e) => {
   e.preventDefault();
   const newName = nameInput.value;
   const newScore = scoreInput.value;
-  submitScores(newName,newScore)
+  await submitScores(newName,newScore);
+  clearfields()
 })
 
