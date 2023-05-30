df32f88f52ebbed7e8f481c9a9a4f4f26659d419
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-05-30 20:33:52 +0200

    Fix bug

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,33 +1,24 @@
-import { getData } from './api.js';
+import { getData } from './api.js'
 
-const leaderboard = document.querySelector('.leaderboard')
-const refreshBtn = document.querySelector('.refresh-btn')
-
-// Function to display the loading message
-function showLoadingMessage() {
-  leaderboard.innerHTML = 'Loading...'
-}
-
+const leaderboard = document.querySelector('.leaderboard');
+const refreshBtn = document.querySelector('.refresh-btn');
 // -- Method to display the scores.
 const displayScores = async () => {
-  refreshBtn.disabled = true
-  showLoadingMessage() // Display the loading message
-
   const scores = await getData()
   const scoresObj = scores.result
   scoresObj.sort((a, b) => b.score - a.score) // Sort by score in descending order
 
   leaderboard.innerHTML = '' // Clear the leaderboard
 
-  for (const scoreObj of scoresObj.slice(0, 40)) {
+  scoresObj.slice(0, 40).forEach((scoreObj) => {
+
       const scoreContainer = document.createElement('div')
       scoreContainer.classList.add('score-container')
-      scoreContainer.innerHTML = `${scoreObj.user}: ${scoreObj.score}`
+      scoreContainer.textContent = `${scoreObj.user}: ${scoreObj.score}`
       leaderboard.appendChild(scoreContainer)
-    }
-  }
+  })
 
   refreshBtn.disabled = false
-
+}
 
 export default displayScores
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,64 +1,76 @@
-import { submitScores } from './api';
-import displayScores from './display';
+import { submitScores } from './api.js'
+import displayScores from './display.js'
 
-const refreshBtn = document.querySelector('.refresh-btn');
-const submitBtn = document.querySelector('.submit-btn');
-const nameInput = document.querySelector('.name-input');
-const scoreInput = document.querySelector('.score-input');
-const msg = document.querySelector('.msg');
-const formC = document.querySelector('.form');
-const form = document.querySelector('.form-container');
-const closeBtn = document.querySelector('.close-form');
-const addBtn = document.querySelector('.add-btn');
+const leaderboard = document.querySelector('.leaderboard')
+const refreshBtn = document.querySelector('.refresh-btn')
+const submitBtn = document.querySelector('.submit-btn')
+const nameInput = document.querySelector('.name-input')
+const scoreInput = document.querySelector('.score-input')
+const msg = document.querySelector('.msg')
+const formC = document.querySelector('.form')
+const form = document.querySelector('.form-container')
+const closeBtn = document.querySelector('.close-form')
+const addBtn = document.querySelector('.add-btn')
+let loadingMessage // Variable to store the loading message element
+
 // Function to display the form
 function displayForm() {
-  form.classList.add('active');
-  closeBtn.style.display = 'block';
+  form.classList.add('active')
+  closeBtn.style.display = 'block'
 }
 
 // Function to hide the form
 function hideForm() {
-  form.classList.remove('active');
-  closeBtn.style.display = 'none';
+  form.classList.remove('active')
+  closeBtn.style.display = 'none'
+}
+
+function showLoadingMessage() {
+  leaderboard.innerHTML = '' // Clear the leaderboard
+  loadingMessage = document.createElement('div')
+  loadingMessage.textContent = 'Loading...'
+  leaderboard.appendChild(loadingMessage)
 }
 
 // Event listener for Add button
-addBtn.addEventListener('click', displayForm);
+addBtn.addEventListener('click', displayForm)
 
 // Event listener for Close button
-closeBtn.addEventListener('click', hideForm);
+closeBtn.addEventListener('click', hideForm)
 
 // Event listener for Refresh button
-refreshBtn.addEventListener('click', displayScores);
+refreshBtn.addEventListener('click', () => {
+  showLoadingMessage() // Display the loading message
+  setTimeout(displayScores, 1500) // Invoke displayScores after 2 seconds
+})
 
 // Event listener for form submission
 formC.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  submitBtn.disabled = true;
-  const newName = nameInput.value.trim();
-  const newScore = Number(scoreInput.value);
-  
+  e.preventDefault()
+  submitBtn.disabled = true
+  const newName = nameInput.value.trim()
+  const newScore = Number(scoreInput.value)
+
   if (newName !== '' && !Number.isNaN(newScore)) {
     try {
-       await submitScores(newName, newScore);
-    msg.style.display = 'block';
-    setTimeout(() => {
-      msg.style.display = 'none';
-    }, 2000);
-    submitBtn.disabled = false;
-    formC.reset();
-    } catch(error) {
+      await submitScores(newName, newScore)
+      msg.style.display = 'block'
+      setTimeout(() => {
+        msg.style.display = 'none'
+      }, 2000)
+      submitBtn.disabled = false
+      formC.reset()
+    } catch (error) {
       console.error('Failed to submit scores:', error)
     }
-   
   } else {
-    msg.style.display = 'block';
-    msg.textContent = 'Please fill all fields';
-    msg.classList.add('error');
+    msg.style.display = 'block'
+    msg.textContent = 'Please fill all fields'
+    msg.classList.add('error')
     setTimeout(() => {
-      msg.style.display = 'none';
-      msg.textContent = 'Your score has been sent successfully';
-      msg.classList.remove('error');
-    }, 2000);
+      msg.style.display = 'none'
+      msg.textContent = 'Your score has been sent successfully'
+      msg.classList.remove('error')
+    }, 2000)
   }
-});
+})
