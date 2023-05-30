34a033766dcb43a2c4bf64797453b4d7fe8bb98b
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-05-30 20:34:41 +0200

    Fix linters

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,24 +1,23 @@
-import { getData } from './api.js'
+import { getData } from './api.js';
 
 const leaderboard = document.querySelector('.leaderboard');
 const refreshBtn = document.querySelector('.refresh-btn');
 // -- Method to display the scores.
 const displayScores = async () => {
-  const scores = await getData()
-  const scoresObj = scores.result
-  scoresObj.sort((a, b) => b.score - a.score) // Sort by score in descending order
+  const scores = await getData();
+  const scoresObj = scores.result;
+  scoresObj.sort((a, b) => b.score - a.score); // Sort by score in descending order
 
-  leaderboard.innerHTML = '' // Clear the leaderboard
+  leaderboard.innerHTML = ''; // Clear the leaderboard
 
   scoresObj.slice(0, 40).forEach((scoreObj) => {
+    const scoreContainer = document.createElement('div');
+    scoreContainer.classList.add('score-container');
+    scoreContainer.textContent = `${scoreObj.user}: ${scoreObj.score}`;
+    leaderboard.appendChild(scoreContainer);
+  });
 
-      const scoreContainer = document.createElement('div')
-      scoreContainer.classList.add('score-container')
-      scoreContainer.textContent = `${scoreObj.user}: ${scoreObj.score}`
-      leaderboard.appendChild(scoreContainer)
-  })
+  refreshBtn.disabled = false;
+};
 
-  refreshBtn.disabled = false
-}
-
-export default displayScores
+export default displayScores;
